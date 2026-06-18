eb705d790315f9b337fa012e82c3d15644124102
maengjh <juhee.bda@gmail.com>
2026-06-18 17:39:03 +0900

    chore: pre-commit 추가

--- auth-service/app/__init__.py
+++ auth-service/app/__init__.py
@@ -4,10 +4,10 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-
 # 앱과 분리해서 선언하고 init_app(app) 으로 나중에 연결
 db = SQLAlchemy()
 migrate = Migrate()
+
 
 # Application Factory 패턴 - 목적: 앱을 함수 호출 시점에 구성하는 것
 def create_app() -> Flask:
@@ -18,7 +18,9 @@ def create_app() -> Flask:
     AUTH_DB_PORT = os.environ.get("AUTH_DB_PORT")
 
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{AUTH_DB_USER}:{AUTH_DB_PASSWORD}@{AUTH_DB_HOST}:{AUTH_DB_PORT}/{AUTH_DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"postgresql://{AUTH_DB_USER}:{AUTH_DB_PASSWORD}@{AUTH_DB_HOST}:{AUTH_DB_PORT}/{AUTH_DB_NAME}"
+    )
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 
     db.init_app(app)
@@ -26,10 +28,11 @@ def create_app() -> Flask:
     migrate.init_app(app, db)
 
     # Alembic 마이그레이션을 위해서 import
-    from app import models
+    from app import models  # noqa: F401
 
     # blueprint import 는 함수 안에서 - circular import 방지
     from app.blueprints.auth import auth_bp
+
     app.register_blueprint(auth_bp, url_prefix="/")
 
     return app
