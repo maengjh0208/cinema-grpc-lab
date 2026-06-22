e0f929de55a5a2e149a89ca1c592a7d67459f6c5
maengjh <juhee.bda@gmail.com>
2026-06-22 18:03:12 +0900

    chore: booking-service - DB 드라이버를 비동기로 전환

--- booking-service/database.py
+++ booking-service/database.py
@@ -1,19 +1,21 @@
 import os
+from typing import AsyncGenerator
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = (
-    f"postgresql://{os.environ['BOOKING_DB_USER']}:{os.environ['BOOKING_DB_PASSWORD']}"
+    f"postgresql+asyncpg://{os.environ['BOOKING_DB_USER']}:{os.environ['BOOKING_DB_PASSWORD']}"
     f"@{os.environ['BOOKING_DB_HOST']}:{os.environ['BOOKING_DB_PORT']}/{os.environ['BOOKING_DB_NAME']}"
 )
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(
+
+engine = create_async_engine(DATABASE_URL)
+SessionLocal = async_sessionmaker(
     bind=engine,
-    autocommit=False,
-    autoflush=False,
     expire_on_commit=False,
+    autoflush=False,
+    # autocommit 은 이제 default False
 )
 
 
@@ -21,13 +23,11 @@ class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-        db.commit()
-    except Exception:
-        # except: 만 하면 프로그램 종료 신호(KeyboardInterrupt, SystemExit 등)까지 가로챔.
-        db.rollback()
-    finally:
-        db.close()
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    # context manager 가 자동으로 db.close() 해줌
+    async with SessionLocal() as db:
+        try:
+            yield db
+            await db.commit()
+        except Exception:  # # except: 만 하면 프로그램 종료 신호(KeyboardInterrupt, SystemExit 등)까지 가로챔.
+            await db.rollback()
