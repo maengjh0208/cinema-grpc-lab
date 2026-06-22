44211fb6513bef660c1735a331e43584291878ba
maengjh <juhee.bda@gmail.com>
2026-06-22 19:00:44 +0900

    chore: ALembic + 비동기 DB 설정

--- Makefile
+++ Makefile
@@ -1,14 +1,37 @@
-# migrations/versions/ 에 있는 마이그레이션 파일들 중 아직 적용 안 된 것들을 DB에 순서대로 적용한다.
-# Alembic 이 내부적으로 alembic_version 테이블을 관리해서 어디까지 적용했는지 추적한다.
-auth-migrate:
-	docker compose run --rm auth-service flask db upgrade
+# ===============================================
+# auth-service
+# ===============================================
+
+# Alembic 을 처음 세팅할 때 딱 한 번 실행함.
+# migrations/ 폴더와 alembic.ini, env.py 등 Alembic 설정 파일들을 생성한다. 이미 migrations/ 가 있으면 다시 실행할 필요 없다.
+auth-migrate-init:
+	docker compose run --rm auth-service flask db init
 
 # 현재 models.py를 읽고 DB 현재 상태와 비교해서 차이점을 마이그레이션 파일로 자동 생성한다.
 # migrations/versions/ 안에 xxxx_설명.py 파일이 만들어지며, 아직 DB에 적용된 상태는 아니다.
 auth-migrate-create:
 	docker compose run --rm auth-service flask db migrate -m "${message}"
 
-# Alembic 을 처음 세팅할 때 딱 한 번 실행함.
-# migrations/ 폴더와 alembic.ini, env.py 등 Alembic 설정 파일들을 생성한다. 이미 migrations/ 가 있으면 다시 실행할 필요 없다.
-auth-migrate-init:
-	docker compose run --rm auth-service flask db init
+# migrations/versions/ 에 있는 마이그레이션 파일들 중 아직 적용 안 된 것들을 DB에 순서대로 적용한다.
+# Alembic 이 내부적으로 alembic_version 테이블을 관리해서 어디까지 적용했는지 추적한다.
+auth-migrate:
+	docker compose run --rm auth-service flask db upgrade
+
+# ===============================================
+# booking-service
+# ===============================================
+
+# booking-service Alembic 초기화
+# migrations/ 폴더와 alembic.ini 파일이 생긴다. alembic.ini 파일에서 sqlalchemy.url 를 주석처리한다. URL은 환경변수에서 읽을것이기 때문.
+booking-migrate-init:
+	docker compose run --rm booking-service alembic init -t async migrations
+
+# DB 현재 상태와 비교해서 차이점을 마이그레이션 파일로 자동 생성
+# 예:  make booking-migrate-create msg="create initial tables"
+booking-migrate-create:
+	docker compose run --rm booking-service alembic revision --autogenerate -m "${message}"
+
+# DB에 적용
+booking-migrate:
+	docker compose run --rm booking-service alembic upgrade head
+
---
+++ booking-service/migrations/env.py
@@ -0,0 +1,101 @@
+import asyncio
+import os
+from logging.config import fileConfig
+
+import models  # noqa: F401
+from alembic import context
+from database import Base
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{os.environ['BOOKING_DB_USER']}:{os.environ['BOOKING_DB_PASSWORD']}"
+    f"@{os.environ['BOOKING_DB_HOST']}:{os.environ['BOOKING_DB_PORT']}/{os.environ['BOOKING_DB_NAME']}"
+)
+# set_main_option 메서드로 alembic.ini 파일의 "sqlalchemy.url" 값을 덮어쓰기
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+# autogenerate 가 테이블을 인식할 수 있도록 모델 연결
+# Base.metadata는 모든 ORM 모델의 테이블 정보를 가지고 있다.
+# import models 가 있어야 Movie, Hall, Seat 등 클래스들이 메모리에 올라가서 Base.metadata에 등록이 된다. 이게 없으면 빈 마이그레이션이 생성됨
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
